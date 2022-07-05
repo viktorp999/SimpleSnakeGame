d07b5be95f8eabb23679846e84f68cc89d83dcb7
viktorp999 <viktorpetrovski99@gmail.com>
2022-07-05 16:54:51 +0200

    Add project files.

---
+++ SimpleSnakeGame/Settings.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleSnakeGame
+{
+    class Settings
+    {
+        public static int Width { get; set; }
+        public static int Height { get; set; }
+        public static string direction;
+
+        public Settings()
+        {
+            Width = 16;
+            Height = 16;
+            direction = "left";
+        }
+    }
+}
